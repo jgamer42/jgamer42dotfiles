0d32a381eb1f8ca31c17dea925deb6eaa9b953c4
Jaime <jgaimer42@gmail.com>
2021-01-04 16:00:58 -0500

    salvando los alias

--- zsh/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e 
-#path power lvl 10k config file /
-#this install zsh and oh my zsh
-sudo apt install zsh
-chsh -s $(which zsh)
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
